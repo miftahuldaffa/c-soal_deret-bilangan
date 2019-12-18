6f0e1ead4fa71c5bfb17d1ea42c015a92e39107b
M Daffa <41532495+md4fv@users.noreply.github.com>
2019-12-18 23:24:04 +0700

    Update nomer2.cpp

--- nomer2.cpp
+++ nomer2.cpp
@@ -1,20 +1,30 @@
+# Author : M Daffa
+# Team   : Black Coder Crush
+
 #include<iostream.h>
-    #include<conio.h>
-    main()
-   {
-   cout<<"program untuk menghitung 10 deret bilangan ganjil"<<endl;
-   int a=1,b=19,n;
-   for(a=a;a<=b;a+=2)
-   {
-   cout<<a;
-   if(a<b)
-   {
+#include<conio.h>
+
+
+main()
+{
+ int a=1,b=19,n;
+
+
+ for(a=a;a<=b;a+=2)
+ {
+  cout<<a;
+  if(a<b)
+  {
    cout<<" + ";
-   }
-   }
-   cout<<" = ";
-   n=(b+1)/2;
-   n=n*n;
-   cout<<n;
-  getch ();
-   }
+  }
+ }
+
+
+ cout<<" = ";
+ n=(b+1)/2;
+ n=n*n;
+ cout<<n;
+
+
+   getch ();
+}
