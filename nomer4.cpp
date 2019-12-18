14543088ce5840c28bfcc6eed3d50507399d19d3
M Daffa <41532495+md4fv@users.noreply.github.com>
2019-12-18 23:29:54 +0700

    Update nomer4.cpp

--- nomer4.cpp
+++ nomer4.cpp
@@ -1,32 +1,27 @@
-   #include <cstdlib>
-   #include <iostream>
-   #include <iomanip>
-   using namespace std;
-   int main(int argc, char *argv[])
-   {
-    int n=11;
-    int baris=n/2;
-    int hasil;
-    int total=0;
-    int k;
-    for (int i=1;i<=baris;i++)
-   {
-        hasil=1;k=1;
-        for(int j=1;j<=i;j++)
-   {
-        hasil=hasil*k;
-        cout<<k;
-        if (j==i)
-        cout<<"  ";
-    else
-        cout<<" * ";
-        k=k+2;      
-    }
-       cout<<setiosflags(ios::right)<<setw(10)<<" = "<<hasil<<endl;
-       total=total+hasil;
-    }
-       cout<<setiosflags(ios::right)<<setw(10)<<" total = "<<total<<endl;
-       cout<<endl;
-       system("PAUSE");
-       return EXIT_SUCCESS;
-    }
+# Author : M Daffa
+# Team   : Black Coder Crush
+
+#include<iostream.h>
+#include<stdio.h>
+#include<conio.h>
+
+
+main()
+{
+int A,i,j,jum;
+cout<<"input :";cin>>A;
+for(i=1;i<=A;i+=2)
+{
+ jum=1;
+ for(j=1;j<=i;j+=2)
+ {
+ if(j!=1)
+ cout<<" * ";
+ cout<<j;
+ jum*=j;
+ }
+cout<<"= "<<jum<<endl;
+}
+cout<<endl;
+getch();
+}
