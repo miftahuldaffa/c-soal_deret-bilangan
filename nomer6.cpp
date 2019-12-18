a201207b738d7f3b03ff7c9093909d680a7dd757
M Daffa <41532495+md4fv@users.noreply.github.com>
2019-12-18 23:15:38 +0700

    Create nomer6.cpp

---
+++ nomer6.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+    int x[10] = {1, 2, 3, 4, 5};
+    int y[10] = {1, 2, 3, 4, 5};
+    int da = 0, DA =1;
+    char ascii;
+
+    endl(cout);
+    cout<<"X/Y";
+    for(int X=1; X<11; ++X)
+    {
+        cout<<"   "<<X<<"  ";
+    }
+
+    endl(cout);
+    endl(cout);
+    //Tampil garis di mulai disini
+    while(da<65)
+    {
+        cout<<(char)196;
+        da++;
+    }
+    //Garis berakhir disini
+    endl(cout);
+
+    for(int a=0; a<10; a++)
+    {endl(cout);
+     cout<<x[a]<<setw(sizeof(x[a]))<<(char)179;
+     for(int b=0; b<10; b++)
+     {
+            cout<<setw(6)<<x[a]*y[b];
+        }
+        endl(cout);
+    }
+
+    endl (cout);
+
+    system("pause");
+}
