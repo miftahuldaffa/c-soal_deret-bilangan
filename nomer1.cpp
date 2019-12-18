0b60d5a9ea1717e3fee1d15101719b15a9b66be0
M Daffa <41532495+md4fv@users.noreply.github.com>
2019-12-18 23:22:44 +0700

    Update nomer1.cpp

--- nomer1.cpp
+++ nomer1.cpp
@@ -1,20 +1,28 @@
+# Author : M Daffa
+# Team   : Black Coder Crush
+
 #include<iostream.h>
-    #include<conio.h>
-     main()
-    {
-    cout<<"program untuk menghitung 10 deret bilangan genap "<<endl;
-    int a=2,b=20,n;
-    for(a=a;a<=b;a+=2)
-    {
-    cout<<a;
-    if(a<b)
-    {
-    cout<<" + ";
-    }
-    }
-    cout<<" = ";
-   n=b/2;
-   n=n*(n+1);
-   cout<<n;
- getch();
-   }
+#include<conio.h>
+
+
+main()
+{
+ int a=2,b=20,n;
+
+ for(a=a;a<=b;a+=2)
+ {                          
+  cout<<a;
+  if(a<b)
+  {
+   cout<<" + ";
+  }
+ }
+
+ cout<<" = ";
+ n=b/2;
+ n=n*(n+1);
+ cout<<n;
+
+   getch();
+
+}
