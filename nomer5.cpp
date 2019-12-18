22d750cf000b97ff6ed0c0911331c9e68bb3c2f1
M Daffa <41532495+md4fv@users.noreply.github.com>
2019-12-18 23:30:55 +0700

    Update nomer5.cpp

--- nomer5.cpp
+++ nomer5.cpp
@@ -1,15 +1,20 @@
-  #include <stdio.h>
-    int main(int argc, const char *argv[]) {
-    int i, j;
-    for(i=0;i<5;puts(""),++i) {
-    int n=0;
-    for(j=5;j>i;n+=2*(j--)) {
-    if(j>i+1) {
-    printf("%d*",2*j);
-    }
-    else {
-    printf("%d ",2*j);
-    }
-    } printf("=%d",n);
-    } return(0);
-    }
+# Author : M Daffa
+# Team   : Black Coder Crush
+
+#include <stdio.h>
+
+
+int main(int argc, const char *argv[]) {
+int i, j;
+for(i=0;i<5;puts(""),++i) {
+int n=0;
+for(j=5;j>i;n+=2*(j--)) {
+if(j>i+1) {
+printf("%d*",2*j);
+}
+else {
+printf("%d ",2*j);
+}
+} printf("=%d",n);
+} return(0);
+}
