edbb4b55fe311ba9b85b223d646508557170f8c3
M Daffa <41532495+md4fv@users.noreply.github.com>
2019-12-18 23:18:43 +0700

    Create nomer3.cpp

---
+++ nomer3.cpp
@@ -0,0 +1,39 @@
+#include<iostream.h>
+#include<conio.h>
+
+
+main()
+{
+ int A,i,j,jum;
+
+
+ for (i=1;i<=4;i++)
+ {
+  jum=0;
+  for (j=1;j<=i;j++)
+  {
+   if (j != 1)
+   cout<<"+";
+   cout<<j;
+   jum +=j;
+  }
+  cout<<"\t\t=  "<<jum<<endl;
+ }
+
+
+ for (i=5;i==5;i++)
+ {
+  jum=0;
+  for (j=1;j<=i;j++)
+  {
+   if (j != 1)
+   cout<<"+";
+   cout<<j;
+   jum +=j;
+  }
+  cout<<"\t=  "<<jum<<endl;
+ }
+
+
+ getch();
+}
